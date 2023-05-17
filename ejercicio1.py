363e27dea9e4f7fabbd6e893d585b0a3704cf568
aaaibd <aaaibd@users.noreply.github.com>
2023-05-17 15:37:37 -0400

    a

---
+++ ejercicio1.py
@@ -0,0 +1,25 @@
+diccionario = {
+    "Región 8":"Bio Bio",
+    "Región 10":"Los lagos",
+    "Superficie 1":"23890",
+    "Superficie 2 ":"48583",
+    "Habitantes 1":"1556805",
+    "Habitantes ":"828708"}
+#Guardar la información de la tabla en un diccionario, luego imprimir el diccionario por consola
+print(diccionario)
+
+#crear clave densidad, densidad = habitantes/superficie
+
+
+
+#agregar clave capital
+diccionario["Capital 1"] = "Concepción"
+diccionario["Capital 2"] = "Puerto Montt"
+
+#agregar clave comunas
+diccionario["Comunas Bio Bio"] = " Lota, Lebu, Los Ángeles"
+diccionario["Comunas Los lagos"] = " Castro, Puerto Varas, Purranque"
+#agregar clave provincia en tuplas
+tuple("Provincias")
+#imprimir diccionario versión final
+print(diccionario)
