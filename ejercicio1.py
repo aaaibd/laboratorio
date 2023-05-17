d6c949ac33d021144f04eefcf0989926354e84da
aaaibd <aaaibd@users.noreply.github.com>
2023-05-17 15:48:42 -0400

    a

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,13 +4,16 @@ diccionario = {
     "Superficie 1":"23890",
     "Superficie 2 ":"48583",
     "Habitantes 1":"1556805",
-    "Habitantes ":"828708"}
+    "Habitantes 2":"828708"}
 #Guardar la información de la tabla en un diccionario, luego imprimir el diccionario por consola
 print(diccionario)
 
 #crear clave densidad, densidad = habitantes/superficie
+d1 = 1556805 / 1556805
+d2 = 828708/ 48583
 
-
+diccionario["Densidad 1"] = f"{d1:.1f}"
+diccionario["Densidad 2"] = f"{d2:.1f}"
 
 #agregar clave capital
 diccionario["Capital 1"] = "Concepción"
@@ -20,6 +23,9 @@ diccionario["Capital 2"] = "Puerto Montt"
 diccionario["Comunas Bio Bio"] = " Lota, Lebu, Los Ángeles"
 diccionario["Comunas Los lagos"] = " Castro, Puerto Varas, Purranque"
 #agregar clave provincia en tuplas
-tuple("Provincias")
+provincia1 = ("Biobío", "Arauco", "Concepción""\n")
+provincia2 = ("Osorno", "Llanquihue", "Chiloé","Palena""\n")
+print(type(provincia1))
+print(type(provincia2))
 #imprimir diccionario versión final
 print(diccionario)
